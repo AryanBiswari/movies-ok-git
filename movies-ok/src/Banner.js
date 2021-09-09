f6c65653de5f3066692b1e3deaf825c5174e5709
Aryan Biswari <aryanbiswari14@gmail.com>
2021-09-09 12:14:14 +0530

    banner + api finished2

--- movies-ok/src/Banner.js
+++ movies-ok/src/Banner.js
@@ -6,18 +6,18 @@ import requests from "./Requests";
 export default function Banner(props) {
   const [movie, setMovie] = useState([]);
 
-    useEffect(() => {
-      async function fetchData() {
-        const request = await axios.get(requests.fetchNetflixOrignals);
-        setMovie(
-          request.data.results[
-            Math.floor(Math.random() * request.data.results.length - 1)
-          ]
-        );
-        return request;
-      }
-      fetchData()
-    }, []);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchTrending);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
@@ -31,13 +31,15 @@ export default function Banner(props) {
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-        {truncate(movie?.overview,150)}
+          {truncate(movie?.overview, 200)}
         </h1>
       </div>
       <div className="banner--fadeBottom" />
