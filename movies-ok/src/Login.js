230da312e21b5f75f72b174e22350e8e88a14b56
Aryan Biswari <aryanbiswari14@gmail.com>
2021-09-11 20:02:57 +0530

    Login Screen Started2

--- movies-ok/src/Login.js
+++ movies-ok/src/Login.js
@@ -7,6 +7,12 @@ function Login() {
             <div className="loginScreen__background">
                 <img className="loginScreen__logo" src={logo} alt="logo"/>
                 <button onClick="" className="loginScreen__button">Sign In</button>
+                <div className="loginScreen__gradient"/>
+            </div>
+            <div className="loginScreen__body">
+                <>
+                    <h1>Hello My name is Aryan</h1>
+                </>
             </div>
         </div>
     )
