3ea730f3a6958bcf49ba36eae8acdc8426f2fb0f
Aryan Biswari <aryanbiswari14@gmail.com>
2021-09-12 14:49:43 +0530

    Login Screen Started3

--- movies-ok/src/Login.js
+++ movies-ok/src/Login.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React from "react";
 import "./Login.css";
 import logo from "./movies-ok-logo.png";
 function Login() {
-    return (
-        <div className="loginScreen">
-            <div className="loginScreen__background">
-                <img className="loginScreen__logo" src={logo} alt="logo"/>
-                <button onClick="" className="loginScreen__button">Sign In</button>
-                <div className="loginScreen__gradient"/>
-            </div>
-            <div className="loginScreen__body">
-                <>
-                    <h1>Hello My name is Aryan</h1>
-                </>
-            </div>
-        </div>
-    )
+  return (
+    <div className="loginScreen">
+      <div className="loginScreen__background">
+        <img className="loginScreen__logo" src={logo} alt="logo" />
+        <button onClick="" className="loginScreen__button">
+          Sign In
+        </button>
+        <div className="loginScreen__gradient" />
+      </div>
+      <div className="loginScreen__body">
+        <>
+          <h1>Unlimited movies, TV shows and more.</h1>
+          <h2>Watch anywhere. Cancel anytime.</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </h3>
+          <div className="loginScreen__input">
+            <form>
+              <input type="email" placeholder="Email Address" />
+              <button className="loginScreen__getStarted">Get Started</button>
+            </form>
+          </div>
+        </>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
