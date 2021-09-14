23c9d50a00bccab9786e095bc82227fdfabf5568
Aryan Biswari <aryanbiswari14@gmail.com>
2021-09-14 11:38:55 +0530

    Sign Up Screen completed and authentication started

--- movies-ok/src/SignupScreen.js
+++ movies-ok/src/SignupScreen.js
@@ -1,12 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import "./SignupScreen.css";
 function SignupScreen() {
-    return (
-        <div className="signup-screen">
-            
-        </div>
-    )
+  return (
+    <div className="signup-screen">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Enter your email address" />
+        <input type="password" placeholder="Enter your password" />
+        <button type="submit">Sign In</button>
+        <h4>
+          <span className="signup-grey">New to Movies Ok?</span>
+          <span className="signup-link"> Sign Up Now</span>
+        </h4>
+      </form>
+    </div>
+  );
 }
 
-export default SignupScreen
-
+export default SignupScreen;
