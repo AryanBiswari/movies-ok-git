7f7ac7b1ce996cd228a9dbfccf1acc6b1cc20bfb
Aryan Biswari <aryanbiswari14@gmail.com>
2021-09-14 10:45:46 +0530

    Login Screen finish and Signin screen started

--- movies-ok/src/Login.js
+++ movies-ok/src/Login.js
@@ -1,8 +1,9 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import logo from "./movies-ok-logo.png";
+import SignupScreen from "./SignupScreen";
 function Login() {
-  const[signIn, setSignIn] = useState(false);
+  const [signIn, setSignIn] = useState(false);
   return (
     <div className="loginScreen">
       <div className="loginScreen__background">
@@ -13,20 +14,29 @@ function Login() {
         <div className="loginScreen__gradient" />
       </div>
       <div className="loginScreen__body">
-        <>
-          <h1>Unlimited movies, TV shows and more.</h1>
-          <h2>Watch anywhere. Cancel anytime.</h2>
-          <h3>
-            Ready to watch? Enter your email to create or restart your
-            membership.
-          </h3>
-          <div className="loginScreen__input">
-            <form>
-              <input type="email" placeholder="Email Address" />
-              <button onClick={() => setSignIn(true)} className="loginScreen__getStarted">Get Started</button>
-            </form>
-          </div>
-        </>
+        {signIn ? (
+          <SignupScreen />
+        ) : (
+          <>
+            <h1>Unlimited movies, TV shows and more.</h1>
+            <h2>Watch anywhere. Cancel anytime.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+            <div className="loginScreen__input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="loginScreen__getStarted"
+                >
+                  Get Started
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
---
+++ movies-ok/src/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SignupScreen() {
+    return (
+        <div className="signup-screen">
+            
+        </div>
+    )
+}
+
+export default SignupScreen
+
