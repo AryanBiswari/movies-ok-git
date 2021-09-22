fd05b3f37b649137bbf655667d82c5eca6fca1f9
Aryan Biswari <aryanbiswari14@gmail.com>
2021-09-22 11:02:44 +0530

    firebase.js added

---
+++ movies-ok/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyByae6YZxL3dBNJJcf2ayi18_xO05Fh3Fk",
+    authDomain: "movies-ok-89113.firebaseapp.com",
+    projectId: "movies-ok-89113",
+    storageBucket: "movies-ok-89113.appspot.com",
+    messagingSenderId: "687415431161",
+    appId: "1:687415431161:web:bb4a752d84e9133ca97728"
+  };
